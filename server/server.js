18f5db1699bb903a6ef93949b2c832479b010f2e
Ck T <chengkou@gmail.com>
2019-06-03 22:50:19 -0500

    add server.js

---
+++ server/server.js
@@ -0,0 +1,19 @@
+let express = require('express');
+let bodyParser = require('body-parser')// goes with req.body ..DO NOT FORGET
+let PORT = 5000;
+let app = express();
+app.use(express.static('server/public'));// get any request, static
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT)
+});//end listen PORT
+
+let calculator = [];
+
+
+app.get('/calculator', (req, res) => {// get info from server
+
+    console.log(calculator);
+    res.send(calculator);
+});
